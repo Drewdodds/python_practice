5abab8652a0db3c4e1c1355289638a41c998086d
Drew Dodds <drewdoddss@gmail.com>
2023-05-08 08:18:32 -0400

    finished varaible scope file

--- corey_shafter_classes/variable_scope.py
+++ corey_shafter_classes/variable_scope.py
@@ -41,17 +41,40 @@ x = 'global x' # The var x here is a global var b/c it is in the main body of ou
 
 # What is we did want to set a new value for the global x var WITHIN the scope of the function? We have to tell python to do that.
 # To do that we add that to the scope of the function as seen re-written below
-def test():
-    global x
-    x = 'local x' # this is a local var to this test function.
-    #print(y) # comment this out for test below.
-    print(x)
+# def test():
+#     global x
+#     x = 'local x' # this is a local var to this test function.
+#     #print(y) # comment this out for test below.
+#     print(x)
 
-test()
-print(x) # this then prints out the global one both times b/c we told python to do so.
+# test()
+# print(x) # this then prints out the global one both times b/c we told python to do so.
 
 # Sometimnes it can be tempting to learn something new and then overuse it. I wanted to show you the global statement so that you know what it does and how to use it. 
 # but I don't think I've ever had the need to uise this myself and if you find yourself using it often then you are probably doing something wrong. 
 # Using the local scope of a function make it easier to understand and easier to work with. So you can imagine how difficult it would be to maintain our coude
 # if you use the global statments and had to worry about vars and your functiosn overriding values outside of that function.
 # So with the local scope being self contained it allows us to not have to worry about what is going on outside of the function.
+
+# Let's now look at the built in scope. It is just names that are preassigned in Python. Example - min() is a built in function that finds the smallest value of a iterable.
+# Example:
+# m = min([5,1,4,2,3])
+# print(m) # we were able to use min() b/c it's a built in function. If we want to veiew the variables within this function we can...
+# import builtins
+# print(dir(builtins)) 
+# one thing you want to be careful of as far as builtins is accidently overriding them. If you were to create a new function called min() you could overright what it is.
+
+# now let's look at enclosing scrope. 
+
+def outer():
+    x = 'outer x' #local x to the outer function
+    def inner():
+        x = 'inner x' #local x to the inner function
+        print(x)
+
+    inner()
+    print(x)
+
+outer() # this prints out the print statement from both functions. BUT if you were to comment out the 'x = 'inner x'' then it would print out outer x twice. 
+# b/c for that first inner() function it's checking local first. If there is not one is goes one step up and looks at anything enclosing - which there is one in the outer() function.
+
