f380649c45f611c72256c8c26de5b325f4947231
Drew Dodds <drewdoddss@gmail.com>
2023-06-07 08:40:13 -0400

    started pandas series

---
+++ corey_shafter_classes/Pandas_Practice/pandas_part1.py
@@ -0,0 +1,10 @@
+# In this vid series we will be learning how to use the pandas python lib. 
+# Pandas is a data analsis lib that allows us to easily read in and analyze data and work with diff data types. 
+# We can use this to analyze csv,excel, etc. 
+# In this vid we will learn how to install pandas and dowload the data and jupyter notebook. 
+
+# Let's install pandas. All we need to do is say "pip install pandas" in the command line
+# we will then install jupyter b/c it goes really well with pandas. It allows us to see our data more easily in the browser to print and vis data. 
+
+# Now we need to download the data we will use for this series. "https://insights.stackoverflow.com/survey"
+
