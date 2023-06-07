50687161ef73779b8d5a2dd565311436fa01ee9b
Drew Dodds <drewdoddss@gmail.com>
2023-06-07 08:40:42 -0400

    started pandas series

--- corey_shafter_classes/Pandas_Practice/pandas_part1.py
+++ corey_shafter_classes/Pandas_Practice/pandas_part1.py
@@ -6,5 +6,10 @@
 # Let's install pandas. All we need to do is say "pip install pandas" in the command line
 # we will then install jupyter b/c it goes really well with pandas. It allows us to see our data more easily in the browser to print and vis data. 
 
-# Now we need to download the data we will use for this series. "https://insights.stackoverflow.com/survey"
+# Now we need to download the data we will use for this series. "https://insights.stackoverflow.com/survey" On this page we can download whateverdata they have.
 
+# We are then going to open the csv file in a Jupyter notebook
+
+# Go to terminal the and navigate to the dir where you put that data
+
+# to start it up you just need to say "jupyter notebook"
