1cb2b0e771b087bb1191be9f35fd6fe01f00a8e8
Drew Dodds <drewdoddss@gmail.com>
2023-10-16 08:44:50 -0400

    Update classes_2.py
    Corrected spelling

--- corey_shafter_classes/classes_in_OOP/classes_2.py
+++ corey_shafter_classes/classes_in_OOP/classes_2.py
@@ -1,9 +1,9 @@
-# In the first vid we went over instance varaibles. Now we will go over class varsiables
-# Class variabels are variable that are shared across all instances of a class. 
-# So while instance variables can unique for each instance like our names and email and pay...
+# In the first vid we went over instance varaibles. Now we will go over class variables
+# Class variables are variables that are shared across all instances of a class. It's a higher level variable that applies to all instances within the same class
+# So while instance variables can be unique for each instance like our names and email and pay...
 # Class varaibles should be the same for each instance. So looking at our employee class below...what kind of data do we want to be shared across all employees?
 # For this example we will say that this will be a standard raise that all empployees will get.
-# To do this you will add the class variable to the top of the code block. 
+# To do this you will add the class variable to the top of the code block. See the two added below before the initiation of the emp instances.
 
 
 class Employee: 
@@ -18,7 +18,7 @@ class Employee:
 
         Employee.num_of_emps += 1 # this will increment the class var by 1 each time we make an employee
 
-    def fullname(self): # Here, we difined a method for this class. Each method within a class automatically takes the instance as the first argument and we're going to always call that "self".
+    def fullname(self): # Here, we defined a method for this class. Each method within a class automatically takes the instance as the first argument and we're going to always call that "self".
         return '{} {}'.format(self.first, self.last)
     
     def apply_raise(self):
